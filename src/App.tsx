c26f830834ddee53a8073be610e32d2e97a05fd6
Mike Buckley <mikebuckley.canada@gmail.com>
2019-02-04 12:00:38 -0800

    Added some sweet code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { runBadFunc } from "./services/bad-func";
 
 class App extends Component {
+
+  private runMethod() {
+    console.log("running bad func");
+    runBadFunc(1,2,4,8);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.runMethod}>Run Bad Func</button>
       </div>
     );
   }
